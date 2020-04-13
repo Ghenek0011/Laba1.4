82fca2f7a7d7f664b2090aea3c4f8c3467a3ba8a
Евгений Жуков <ezvgeniy0011@gmail.com>
2020-04-13 11:15:51 +0300

    Добавьте файлы проекта.

---
+++ Laba1.4/Laba1.4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include < math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x, c, S;
+    int n, k;
+    cout << "x= ";
+    cin >> x;
+    cout << endl;
+    cout << "n = ";
+    cin >> n;
+    cout << endl;
+    S = 0;
+    for (k = 1; k <= n; k++)
+    {
+        c = (k * k + x * x * x) / 2;
+        S = S + c;
+    }
+    cout << "Сумма равна " << S << endl;
+    return 0;
+}
+
